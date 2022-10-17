89ff5d0fafb936c835429ebac870772aa16e74c2
Unique Chhetri (Student) <uchhetri@sundevils.asu.edu>
2022-10-16 18:46:45 -0700

    user auhtnetication implementation

--- src/Facade.java
+++ src/Facade.java
@@ -1,2 +1,63 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
 public class Facade {
+    private int userType;
+    private int user;
+    boolean userLogin() {
+
+        // UserName input
+        System.out.println("Please enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine().trim();
+
+        // Password input
+        System.out.println("Please enter your password: ");
+        String userPassword = scanner.nextLine().trim();
+
+        //Match username and password from file
+        HashMap<String, String> buyerUserPassword = new HashMap<String, String>();
+        HashMap<String, String> sellerUserPassword = new HashMap<String, String>();
+
+        try {
+            File buyerDetails = new File(
+                    "BuyerInfo.txt");
+            BufferedReader buffRead
+                    = new BufferedReader(new FileReader(buyerDetails));
+            String text;
+
+            while ((text = buffRead.readLine()) != null) {
+                String[] splitArr = text.split(":");
+                buyerUserPassword.put(splitArr[0], splitArr[1]);
+            }
+
+            File sellerDetails = new File("SellerInfo.txt");
+            buffRead
+                    = new BufferedReader(new FileReader(sellerDetails));
+
+            while ((text = buffRead.readLine()) != null) {
+                String[] splitArr = text.split(":");
+                sellerUserPassword.put(splitArr[0], splitArr[1]);
+            }
+
+            //set user type
+            if (buyerUserPassword.containsKey(username) && buyerUserPassword.get(username).equals(userPassword)) {
+                userType = 0;
+                return true;
+            } else if (sellerUserPassword.containsKey(username) && sellerUserPassword.get(username).equals(userPassword)) {
+                userType = 1;
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            System.err.println("There is an error reading the file" + e.getMessage());
+        }
+
+
+        return false;
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Facade facade = new Facade();
+
+        // User Authentication
+        boolean isValidUser = facade.userLogin();
+        if(isValidUser) {
+            System.out.println("Welcome. Succesfully Logged In");
+        } else {
+            System.out.println("Sorry, you are not registered");
+        }
+
+    }
+}
